d95e791006ec86bb692d379a651103b17f4afc81
John Rom <john@nimblelight.com>
2017-09-15 14:51:00 -0400

    Moved WP install to init

--- hooks/install.sh
+++ hooks/install.sh
@@ -14,26 +14,7 @@ nimble_wp_install(){
 
     local url="$project.$tld"
 
-    echo "Installing WP to $dir -> $url"
-
-    ask title "Site Title" "$project"
-    ask user "Admin User" "admin" --required
-    ask password "Admin Password" "password" --required
-    ask email "Admin Email" "$(git config user.email)" --required
-
-    echo "Waiting for WordPress at $dir/wp-config.php"
-    while [ ! -f $dir/wp-config.php ]
-    do
-        sleep 2
-    done
-
-    echo "Installing WordPress. This could take a few seconds..."
-
-    until wp "$project" core install --path="'$inner_dir'" --url="'$url'" --title="'$title'" --admin_user="'$user'" --admin_password="'$password'" --admin_email="'$email'" --skip-email
-    do
-        echo "WP Install Failed. Retrying..."
-        sleep 2
-    done
+    # Todo, installing WP moved to init hook
 }
 
 nimble_wp_install "$project"
